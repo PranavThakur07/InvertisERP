0d86c70b3f778376282753abac9bed02459253b0
Pranav Thakur <pranavthakur936@gmail.com>
2024-12-11 11:26:15 +0530

    Commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: InAppWebView(
+        initialUrlRequest: URLRequest(url: Uri.parse('http://erp.invertisuniversity.ac.in:81/loginForm.aspx')), // Set the initial URL for your website
+      ),
+    );
+  }
+}
